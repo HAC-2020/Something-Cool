8d597fced27453e51c1ba99b207f232130c3cb7a
danielkline10 <danielkline@Daniels-MBP-2.home>
2020-08-08 22:57:01 -0400

    third commit

---
+++ node_modules/.cache/gh-pages/github.com!HAC-2020!Something-Cool.git/precache-manifest.a2424be584c52b64325224b4ad14b5a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a601bb5d06d4ddb19ceef0d3d20aa05",
+    "url": "/Something-Cool/index.html"
+  },
+  {
+    "revision": "2195576e2f9f60889922",
+    "url": "/Something-Cool/static/css/main.f5466303.chunk.css"
+  },
+  {
+    "revision": "f461891a0168e76970f2",
+    "url": "/Something-Cool/static/js/2.086afe90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Something-Cool/static/js/2.086afe90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2195576e2f9f60889922",
+    "url": "/Something-Cool/static/js/main.c69ce339.chunk.js"
+  },
+  {
+    "revision": "a2af52b00dbebd28a0bf",
+    "url": "/Something-Cool/static/js/runtime-main.9076193f.js"
+  }
+]);
